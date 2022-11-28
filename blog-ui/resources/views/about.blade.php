c776b98e9649daf12c8bea3d5170287133bcf522
melanchorilla-portfolio <dickykamaludin12@gmail.com>
2022-11-28 17:51:13 +0700

    first commit

---
+++ blog-ui/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.blog')
+@section('content')
+
+<div class="jumbotron" id="home">
+    <div class="container">
+    <div class="text-center my-5">
+        <img src="/img/dicky.jpg" class="rounded-circle img-thumbnail" width="25%">
+        <h1 class="display-4">Dicky Kamaludin Bashar</h1>
+        <h3 class="lead">Student | Programmer</h3>
+    </div>
+    </div>
+</div>
+
+@endsection
